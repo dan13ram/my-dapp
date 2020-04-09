022ce13eba9946d9907c98aea3ac77c4cc538559
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-04-09 22:59:48 +0530

    added tests

---
+++ test/DataStore.test.js
@@ -0,0 +1,34 @@
+const DataStore = artifacts.require("DataStore");
+
+contract('DataStore', (accounts) => {
+    before(async () => {
+        this.dataStore = await DataStore.deployed();
+    });
+
+    it('deploys successfully', async () => {
+        const address = await this.dataStore.address;
+        assert.notEqual(address, 0x0);
+        assert.notEqual(address, '');
+        assert.notEqual(address, null);
+        assert.notEqual(address, undefined);
+    });
+
+    it('gets data', async () => {
+        const data = await this.dataStore.data();
+        assert.notEqual(data, '');
+        assert.notEqual(data, null);
+        assert.notEqual(data, undefined);
+        assert.equal(data, 'Default Data');
+    });
+
+    it('sets data', async () => {
+        const input = "input";
+        await this.dataStore.set(input);
+        const data = await this.dataStore.data();
+        assert.notEqual(data, '');
+        assert.notEqual(data, null);
+        assert.notEqual(data, undefined);
+        assert.notEqual(data, 'Default Data');
+        assert.equal(data, input);
+    });
+});
